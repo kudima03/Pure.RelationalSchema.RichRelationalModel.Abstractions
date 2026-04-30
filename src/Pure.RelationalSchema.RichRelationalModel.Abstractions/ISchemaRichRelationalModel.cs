2115d392bc7a7a08e485108bf93ea3e731854787
Dmitry Kurochkin <kudima03@gmail.com>
2026-04-30 10:52:24 +0300

    ISchemaRichRelationalModel implemented

---
+++ src/Pure.RelationalSchema.RichRelationalModel.Abstractions/ISchemaRichRelationalModel.cs
@@ -0,0 +1,6 @@
+using Pure.RelationalSchema.Abstractions.Schema;
+using Pure.RelationalSchema.RelationalModel.Abstractions;
+
+namespace Pure.RelationalSchema.RichRelationalModel.Abstractions;
+
+public interface ISchemaRichRelationalModel : ISchema, ISchemaRelationalModel;
