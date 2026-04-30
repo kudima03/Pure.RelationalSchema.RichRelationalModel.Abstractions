be5e465b04cc91dc344135f39a8ca64bd5ecb5bc
Dmitry Kurochkin <kudima03@gmail.com>
2026-04-30 10:52:07 +0300

    IForeignKeyRichRelationalModel implemented

---
+++ src/Pure.RelationalSchema.RichRelationalModel.Abstractions/IForeignKeyRichRelationalModel.cs
@@ -0,0 +1,6 @@
+using Pure.RelationalSchema.Abstractions.ForeignKey;
+using Pure.RelationalSchema.RelationalModel.Abstractions;
+
+namespace Pure.RelationalSchema.RichRelationalModel.Abstractions;
+
+public interface IForeignKeyRichRelationalModel : IForeignKey, IForeignKeyRelationalModel;
