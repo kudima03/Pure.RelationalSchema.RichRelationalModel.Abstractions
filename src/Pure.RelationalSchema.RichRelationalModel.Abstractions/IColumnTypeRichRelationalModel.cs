9f120ec8a99c96f1482ddd4a0d0789c1c3627b11
Dmitry Kurochkin <kudima03@gmail.com>
2026-04-30 10:52:00 +0300

    IColumnTypeRichRelationalModel implemented

---
+++ src/Pure.RelationalSchema.RichRelationalModel.Abstractions/IColumnTypeRichRelationalModel.cs
@@ -0,0 +1,6 @@
+using Pure.RelationalSchema.Abstractions.ColumnType;
+using Pure.RelationalSchema.RelationalModel.Abstractions;
+
+namespace Pure.RelationalSchema.RichRelationalModel.Abstractions;
+
+public interface IColumnTypeRichRelationalModel : IColumnType, IColumnTypeRelationalModel;
