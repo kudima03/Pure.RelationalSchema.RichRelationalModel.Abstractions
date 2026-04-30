2132da5aa4744a730039473c91748480cc67ee9e
Dmitry Kurochkin <kudima03@gmail.com>
2026-04-30 10:52:15 +0300

    IIndexRichRelationalModel implemented

---
+++ src/Pure.RelationalSchema.RichRelationalModel.Abstractions/IIndexRichRelationalModel.cs
@@ -0,0 +1,6 @@
+using Pure.RelationalSchema.Abstractions.Index;
+using Pure.RelationalSchema.RelationalModel.Abstractions;
+
+namespace Pure.RelationalSchema.RichRelationalModel.Abstractions;
+
+public interface IIndexRichRelationalModel : IIndex, IIndexRelationalModel;
